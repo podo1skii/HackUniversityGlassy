75e3eb0185f8cc5e7ffc781a21bfdb7a7389bd7c
Roman Podolskii <roman_pod27@mail.ru>
2019-03-22 23:09:30 +0300

    Updates

---
+++ precache-manifest.afb55590a7fb6a5cc146a3403faa744d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "95abeda8b9eb6b7ec311",
+    "url": "/HackUniversityGlassy/static/css/main.5737b191.chunk.css"
+  },
+  {
+    "revision": "95abeda8b9eb6b7ec311",
+    "url": "/HackUniversityGlassy/static/js/main.3344fad3.chunk.js"
+  },
+  {
+    "revision": "8d6f0018118ba83a21d6",
+    "url": "/HackUniversityGlassy/static/js/runtime~main.71f94ddc.js"
+  },
+  {
+    "revision": "27244a514112506cf875",
+    "url": "/HackUniversityGlassy/static/css/2.d329aeec.chunk.css"
+  },
+  {
+    "revision": "27244a514112506cf875",
+    "url": "/HackUniversityGlassy/static/js/2.9b9e14f6.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/HackUniversityGlassy/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "753dd932a492120e74b205868040374d",
+    "url": "/HackUniversityGlassy/index.html"
+  }
+];
