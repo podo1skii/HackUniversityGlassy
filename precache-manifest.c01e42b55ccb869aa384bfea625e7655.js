da2792ea3488e07fa749cf6295d130345fe6fdb4
Roman Podolskii <roman_pod27@mail.ru>
2019-03-23 12:36:57 +0300

    Updates

---
+++ precache-manifest.c01e42b55ccb869aa384bfea625e7655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ff8f4a2205eb269951c",
+    "url": "/HackUniversityGlassy/static/css/main.f00721b1.chunk.css"
+  },
+  {
+    "revision": "3ff8f4a2205eb269951c",
+    "url": "/HackUniversityGlassy/static/js/main.b6b6498c.chunk.js"
+  },
+  {
+    "revision": "8d6f0018118ba83a21d6",
+    "url": "/HackUniversityGlassy/static/js/runtime~main.71f94ddc.js"
+  },
+  {
+    "revision": "d6e423722e71a3a36671",
+    "url": "/HackUniversityGlassy/static/css/2.d329aeec.chunk.css"
+  },
+  {
+    "revision": "d6e423722e71a3a36671",
+    "url": "/HackUniversityGlassy/static/js/2.9cd53b8f.chunk.js"
+  },
+  {
+    "revision": "4399b4c913d3ee4c2064633aa0e33d70",
+    "url": "/HackUniversityGlassy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/HackUniversityGlassy/precache-manifest.9cc020a7cb18b10a41376af5bbe15073.js"
+  "/HackUniversityGlassy/precache-manifest.c01e42b55ccb869aa384bfea625e7655.js"
 );
 
 workbox.clientsClaim();
