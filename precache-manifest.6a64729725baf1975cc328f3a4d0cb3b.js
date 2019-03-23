1361f381cc2b68d11c53d17ce179b772fcfce545
Roman Podolskii <roman_pod27@mail.ru>
2019-03-23 03:50:36 +0300

    Updates

--- precache-manifest.6a64729725baf1975cc328f3a4d0cb3b.js
+++ precache-manifest.6a64729725baf1975cc328f3a4d0cb3b.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "410704e44dde2fbd8888",
-    "url": "/HackUniversityGlassy/static/js/main.98d6f85d.chunk.js"
+    "revision": "0cd8e253f5b072f7faa7",
+    "url": "/HackUniversityGlassy/static/js/main.b8085221.chunk.js"
   },
   {
     "revision": "8d6f0018118ba83a21d6",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/HackUniversityGlassy/static/js/2.c4205534.chunk.js"
   },
   {
-    "revision": "f2dd0594717646a417664514ed7b24df",
+    "revision": "a02aa57b7236e690253dee83cca13f11",
     "url": "/HackUniversityGlassy/index.html"
   }
 ];
